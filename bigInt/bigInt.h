7c597164f6579eccf271a6cb5d4731c2a8a79d9d
jacob-halter2 <jacob.halter@wsu.edu>
2021-10-26 12:44:57 -0700

    Add function complete

--- bigInt/bigInt.h
+++ bigInt/bigInt.h
@@ -5,45 +5,69 @@
 class bigInt
 {
 public:
+	bigInt(int x);
 	bigInt();
 	~bigInt();
 	
 	bigInt operator+(bigInt& rhs);
+	//bigInt operator=(bigInt& lhs) { lhs.data = this->data; }
+
+	void printBigInt();
 
 private:
 	vector<int> data;
 };
 
+bigInt::bigInt(int x)
+{
+	data.clear();
+	while (x)
+	{
+		data.push_back(x % 1000);
+		x = x / 1000;
+	}
+}
+
 bigInt::bigInt()
 {
 	data.clear();
-	
 }
 
 bigInt::~bigInt()
 {
 }
 
-inline bigInt bigInt::operator+(bigInt& rhs)
+bigInt bigInt::operator+(bigInt& rhs)
 {
 	bigInt newBigInt;
 	int remainder = 0;
 	int sum = 0;
-	for (int i = 0; i < this->data.size() && i < rhs.data.size(); i++)
+	for (int i = 0; i < data.size() || i < rhs.data.size() || remainder >= 1 ; i++)
 	{
-		if (this->data[i])
+		if (i < data.size())
 		{
-			if (rhs.data[i])
-			{
-				sum = this->data[i] + rhs.data[i] + remainder;
-				newBigInt.data[i] = sum % 1000;
-				remainder = sum / 1000;
-			}
+			if (i < rhs.data.size())
+				sum = data[i] + rhs.data[i] + remainder;
 			else
-			{
-				
-			}
+				sum = data[i] + remainder;
 		}
+		else
+		{
+			if (i < rhs.data.size())
+				sum = rhs.data[i] + remainder;
+			else
+				sum = remainder;
+		}
+		newBigInt.data.push_back(sum % 1000);
+		remainder = sum / 1000;
 	}
 	return newBigInt;
+}
+
+void bigInt::printBigInt()
+{
+	for (int i = data.size()-1; i >= 0; i--)
+	{
+		cout << data[i];
+	}
 }
--- bigInt/bigIntTest.cpp
+++ bigInt/bigIntTest.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 int main()
 {
-	bigInt nFactorial = 1;
+	bigInt num1(123450);
+	bigInt num2(35);
+	bigInt num3; 
+	num3 = num1 + num2;
+	num1.printBigInt();
+	num2.printBigInt();
+	num3.printBigInt();
 
+	return 0;
 }
